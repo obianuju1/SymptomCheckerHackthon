a2633255d64f7ffc27fc7b5a368398eccc3bb19b
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2024-12-28 12:56:56 -0500

    fixed shadcn import problem

--- medify_ai/src/app/page.tsx
+++ medify_ai/src/app/page.tsx
@@ -2,16 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* Navigation Button */}
-        <Link
-          href="/login"
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-        >
-          Go to Login Page
-        </Link>
-      </main>
-    </div>
+    <header>
+      <h1>Medify AI</h1>
+      <p>Diagnosing patients with just a few clicks !</p>
+      <Link href={'/signup'}>Sign Up</Link>
+    </header>
   );
 }
