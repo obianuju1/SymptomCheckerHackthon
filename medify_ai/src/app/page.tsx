277f148ee16933141229a7ffbb6ff4c6f3e93371
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2024-12-28 14:23:19 -0500

    working on home page

--- medify_ai/src/app/page.tsx
+++ medify_ai/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <header>
+    <header className=" w-screen h-screen bg-center bg-cover bg-no-repeat " style={{backgroundImage: "url('/Home_Background_Image.jpg')"}}>
       <h1>Medify AI</h1>
       <p>Diagnosing patients with just a few clicks !</p>
-      <Link href={'/signup'}>Sign Up</Link>
+      <div>
+      <Button asChild><Link href={'/register'}>Sign Up</Link></Button>
+      <Link href={'/sign-in'}>Existing user ? Log in </Link>
+      </div>
     </header>
   );
 }
