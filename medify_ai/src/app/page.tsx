dd29851554771be0b50561cb3d1b0398a613aed4
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2024-12-28 18:14:04 -0500

    added home page that should link to register and login pages

--- medify_ai/src/app/page.tsx
+++ medify_ai/src/app/page.tsx
@@ -3,12 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <header className=" w-screen h-screen bg-center bg-cover bg-no-repeat " style={{backgroundImage: "url('/Home_Background_Image.jpg')"}}>
-      <h1>Medify AI</h1>
-      <p>Diagnosing patients with just a few clicks !</p>
+    <header className=" w-screen h-screen bg-center bg-cover bg-no-repeat bg-black/40 bg-blend-darken flex flex-col items-center justify-center text-white gap-2" style={{backgroundImage: "url('/Home_Background_Image.jpg')"}}>
+      <div className="text-center ">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Medify AI</h1>
+        <h3 className="scroll-m-20 text-l tracking-tight">Diagnosing patients with just a few clicks </h3>
+      </div>
       <div>
-      <Button asChild><Link href={'/register'}>Sign Up</Link></Button>
-      <Link href={'/sign-in'}>Existing user ? Log in </Link>
+      <Button asChild className="block w-[50%] mb-2 mx-auto rounded-lg bg-green-500 hover:bg-green-500"><Link href={'/register'} className="text-center px-0">Sign Up</Link></Button>
+      <Link href={'/login'} className="text-xs hover:underline">Existing user ? Log in </Link>
       </div>
     </header>
   );
