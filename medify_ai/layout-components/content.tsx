a162d707b7f858ffcfadc6c56a85da59e8bdbc65
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2025-01-04 13:43:44 -0500

    added a table to the dashboard

--- medify_ai/layout-components/content.tsx
+++ medify_ai/layout-components/content.tsx
@@ -1,8 +1,38 @@
 import React from "react";
-import { Card } from "@/components/ui/card";
+import { Card, CardHeader } from "@/components/ui/card";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  
 const Content = () => {
   return(
-    <Card className=" content w-[80%] my-0 mx-auto bg-[#292E33] border-none shadow-lg" style={{width: 650}}></Card>
+    <div className="content mt-0 mb-3 mx-auto">
+    <Table className=" table-auto w-full mt-0 mb-2 mx-auto">
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Description</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+</div>
+    
   )
     
   ;
--- medify_ai/src/app/layout.tsx
+++ medify_ai/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../../context/AuthContext";
+import { ThemeProvider } from "next-themes"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <AuthProvider>
-        {children}
+          
+            {children}
         </AuthProvider>
       </body>
     </html>
