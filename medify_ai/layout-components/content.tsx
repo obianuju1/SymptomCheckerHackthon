fdbdb2f29b66e6eb1698c0da2f3f8286fbe72fea
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2025-01-04 12:49:50 -0500

    working on dashboard and commented out authentication redirects

---
+++ medify_ai/layout-components/content.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card } from "@/components/ui/card";
+const Content = () => {
+  return(
+    <Card className=" content w-[80%] my-0 mx-auto bg-[#292E33] border-none shadow-lg" style={{width: 650}}></Card>
+  )
+    
+  ;
+};
+
+export default Content;
---
+++ medify_ai/layout-components/map.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Map = () => {
+  return <div className="message">Map</div>;
+};
+
+export default Map;
